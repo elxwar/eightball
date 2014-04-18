8a9c28cd15b717f64a53da37b537dfaa613e94fa
Eric Waring <ericw@the-feel.co.uk>
2014-04-18 16:30:07 +0100

    v1

--- EightBall/EBViewController.h
+++ EightBall/EBViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface EBViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *answerView;
 
 @end
--- EightBall/EBViewController.m
+++ EightBall/EBViewController.m
@@ -12,7 +12,56 @@
 
 @end
 
+static NSString* gAnswers[] = {
+    @"\rYES",
+    @"\rNO",
+    @"\rMAYBE",
+    @"I\rDON'T\rKNOW",
+    @"TRY\rAGAIN\rSOON",
+    @"\rRTFM"
+};
+
+#define kNumberOfAnswers (sizeof(gAnswers)/sizeof(NSString*))
+
+@interface EBViewController ()
+- (void)fadeFortune;
+- (void)newFortune;
+@end
+
 @implementation EBViewController
+
+- (void)fadeFortune
+{
+    [UIView animateWithDuration:0.75 animations:^{
+        self.answerView.alpha = 0.0;
+    }];
+}
+
+- (void)newFortune
+{
+    self.answerView.text = gAnswers[arc4random_uniform(kNumberOfAnswers)];
+    [UIView animateWithDuration:2.0 animations:^{
+        self.answerView.alpha = 1.0;
+    }];
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion==UIEventSubtypeMotionShake)
+        [self fadeFortune];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion==UIEventSubtypeMotionShake)
+        [self newFortune];
+}
+
+- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion==UIEventSubtypeMotionShake)
+        [self newFortune];
+}
 
 - (void)viewDidLoad
 {
